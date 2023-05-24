fc7c20dd1a592bdb30bef7f7fb77b8a3f9793553
106 <106@academicos.cesde.edu.co>
2023-05-24 11:44:07 -0500

    se crean proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { EmpresaComponent } from './empresa/empresa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    FooterComponent,
+    EmpresaComponent
   ],
   imports: [
     BrowserModule,
